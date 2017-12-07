17b9068a3cd01cc7a20a8e08b52e28fbc1239923
Japa Mala Illo <japa.mala.illo@gmail.com>
2017-12-07 13:06:24 +0530

    Made basic chunk classes

---
+++ voxReader/Chunk.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace voxReader
+{
+    class Chunk
+    {
+        public string ChunkID { get; }
+
+        public List<Chunk> Children;
+    }
+}
